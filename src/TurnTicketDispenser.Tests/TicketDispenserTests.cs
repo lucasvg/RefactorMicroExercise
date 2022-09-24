506e7a90a17a52fd099db0f602df18de7fd04ef8
Lucas Vieira <luvi@bitcoinsuisse.com>
2022-09-24 22:23:41 +0200

    Adds nunit framework version + Introduces TicketDispenserTests

---
+++ src/TurnTicketDispenser.Tests/TicketDispenserTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace TDDMicroExercises.TurnTicketDispenser.Tests
+{
+    [TestFixture]
+    public class TicketDispenserTests
+    {
+        [Test]
+        public void FirstThreeTickets()
+        {
+            var ticketDispenser = new TicketDispenser();
+            var ticket1 = ticketDispenser.GetTurnTicket();
+            Assert.AreEqual(0, ticket1.TurnNumber);
+            var ticket2 = ticketDispenser.GetTurnTicket();
+            Assert.AreEqual(1, ticket2.TurnNumber);
+            var ticket3 = ticketDispenser.GetTurnTicket();
+            Assert.AreEqual(2, ticket3.TurnNumber);
+        }
+    }
+}
